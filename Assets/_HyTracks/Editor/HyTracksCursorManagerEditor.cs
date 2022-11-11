dcedc99bbe5e6d5e4ce0fc6678ee893d7f32b3a0
Dmitry Alexandrovsky <dimi@uni-bremen.de>
2022-11-11 16:31:37 +0100

    first commit

---
+++ Assets/_HyTracks/Editor/HyTracksCursorManagerEditor.cs
@@ -0,0 +1,67 @@
+/*
+ * @author Valentin Simonov / http://va.lent.in/
+ */
+
+using TouchScript.Behaviors.Cursors;
+using UnityEditor;
+using UnityEngine;
+using TouchScript.Editor.EditorUI;
+
+namespace HyTracks.Editor {
+
+    [CustomEditor(typeof(HyTracksCursorManager))]
+    internal sealed class HyTracksCursorManagerEditor:UnityEditor.Editor {
+
+        public static readonly GUIContent TEXT_DPI_HEADER = new GUIContent("Use DPI","Scale touch pointer based on DPI.");
+        public static readonly GUIContent TEXT_CURSORS_HEADER = new GUIContent("Cursors","Cursor prefabs used for different pointer types.");
+        public static readonly GUIContent TEXT_POINTER_SIZE = new GUIContent("Pointer size (cm)","Pointer size in cm based on current DPI.");
+        public static readonly GUIContent TEXT_POINTER_PIXEL_SIZE = new GUIContent("Pointer size (px)","Pointer size in pixels.");
+
+        private SerializedProperty mousePointerProxy, touchPointerProxy, penPointerProxy, objectPointerProxy;
+        private SerializedProperty useDPI, cursorSize, cursorPixelSize;
+        private SerializedProperty cursorsProps;
+
+        private void OnEnable()
+        {
+            mousePointerProxy = serializedObject.FindProperty("mouseCursor");
+            touchPointerProxy = serializedObject.FindProperty("touchCursor");
+            penPointerProxy = serializedObject.FindProperty("penCursor");
+            objectPointerProxy = serializedObject.FindProperty("objectCursor");
+
+            useDPI = serializedObject.FindProperty("useDPI");
+            cursorSize = serializedObject.FindProperty("cursorSize");
+            cursorPixelSize = serializedObject.FindProperty("cursorPixelSize");
+
+            cursorsProps = serializedObject.FindProperty("cursorsProps");
+        }
+
+        public override void OnInspectorGUI()
+        {
+            serializedObject.Update();
+
+            GUILayout.Space(5);
+
+            EditorGUILayout.PropertyField(useDPI,TEXT_DPI_HEADER);
+            if(useDPI.boolValue) {
+                EditorGUILayout.PropertyField(cursorSize,TEXT_POINTER_SIZE);
+            } else {
+                EditorGUILayout.PropertyField(cursorPixelSize,TEXT_POINTER_PIXEL_SIZE);
+            }
+
+            var display = GUIElements.Header(TEXT_CURSORS_HEADER,cursorsProps);
+            if(display) {
+                EditorGUI.indentLevel++;
+                EditorGUILayout.BeginHorizontal();
+                EditorGUILayout.TextField("ID");
+                EditorGUILayout.PropertyField(mousePointerProxy,new GUIContent("Mouse Pointer Proxy"));
+                EditorGUILayout.EndHorizontal();
+                EditorGUILayout.PropertyField(touchPointerProxy,new GUIContent("Touch Pointer Proxy"));
+                EditorGUILayout.PropertyField(penPointerProxy,new GUIContent("Pen Pointer Proxy"));
+                EditorGUILayout.PropertyField(objectPointerProxy,new GUIContent("Object Pointer Proxy"));
+                EditorGUI.indentLevel--;
+            }
+
+            serializedObject.ApplyModifiedProperties();
+        }
+    }
+}
---
+++ Assets/_HyTracks/Scripts/HyTracksObjectCursor.cs
@@ -0,0 +1,12 @@
+using System.Text;
+using TouchScript.Pointers;
+using UnityEngine;
+
+using TouchScript.Behaviors.Cursors;
+namespace HyTracks {
+
+    public class HyTracksObjectCursor: ObjectCursor {
+        
+        
+    }
+}
